620c4ef44668af48453f84726a00123d11f84058
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2025-03-12 03:36:07 +0700

    change

--- resources/views/admin/dashboard_index.blade.php
+++ resources/views/admin/dashboard_index.blade.php
@@ -69,7 +69,7 @@
     <div class="row">
 
         <!-- Area Chart -->
-        <div class="col-xl-8">
+        <div class="col-xl-6">
             <div class="card shadow mb-4">
                 <div class="card-header py-3">
                     <h6 class="m-0 font-weight-bold text-primary">Chart Antrian Online</h6>
