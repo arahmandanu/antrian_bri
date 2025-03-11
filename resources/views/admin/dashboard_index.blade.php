2113d9df4064344e41e5dea0b8d4b4121c609a27
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2025-03-12 03:36:41 +0700

    hide unused

--- resources/views/admin/dashboard_index.blade.php
+++ resources/views/admin/dashboard_index.blade.php
@@ -29,7 +29,7 @@
             </div>
         </div>
 
-        <div class="col-xl-3 col-md-6 mb-4">
+        {{-- <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
@@ -44,7 +44,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
 
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
