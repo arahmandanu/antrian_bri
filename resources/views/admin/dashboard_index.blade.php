7066380e174950286192dc963af50fb2f647ae41
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2025-03-12 03:34:20 +0700

    tes

--- resources/views/admin/dashboard_index.blade.php
+++ resources/views/admin/dashboard_index.blade.php
@@ -144,7 +144,7 @@
                         }],
                     },
                     legend: {
-                        display: false
+                        display: true
                     },
                     tooltips: {
                         titleMarginBottom: 10,
